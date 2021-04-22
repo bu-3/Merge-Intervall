32755e1dd3977d83ef9ceded63a0f14a05403c60
bu-3 <40634763+bu-3@users.noreply.github.com>
2021-04-22 14:46:46 +0200

    Created a simple UI
    Simple to use and easy to understand UI.
    * Add Button
    * Add TextBox for displaying path
    * Add RichTextbox for displaying results

--- Merge-Intervall/MergeIntervallView.cs
+++ Merge-Intervall/MergeIntervallView.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Merge_Intervall
 {
-    public partial class Form1 : Form
+    public partial class MergeIntervallView : Form
     {
-        public Form1()
+        public MergeIntervallView()
         {
             InitializeComponent();
         }
--- Merge-Intervall/Program.cs
+++ Merge-Intervall/Program.cs
@@ -16,7 +16,7 @@ namespace Merge_Intervall
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MergeIntervallView());
         }
     }
 }
